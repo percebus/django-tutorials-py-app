e92e2bc7b49814787ef8d4e2c9a67fcf92573efe
JC Guerrero <JCystems@gMail.com>
2022-07-18 11:37:49 -0500

    refactor(settings.py)
    PostgreSQL

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -70,13 +71,29 @@ TEMPLATES = [
 WSGI_APPLICATION = 'webapp.wsgi.application'
 
 
+# SRC: https://www.postgresql.org/docs/current/libpq-envars.html
+postgreSQL = {
+    'hostaddr': os.environ['PGHOSTADDR'],
+    'port': os.environ['PGPORT'],
+    'user': os.environ['PGUSER'],
+    'password': os.environ['PGPASSWORD'],
+    'dbname': os.environ['PGDATABASE']
+}
+
+# TODO log postgreSQL
+
+
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': postgreSQL['hostaddr'],
+        'PORT': postgreSQL['port'],
+        'USER': postgreSQL['user'],
+        'PASSWORD': postgreSQL['password'],
+        'NAME': postgreSQL['dbname']
     }
 }
 
