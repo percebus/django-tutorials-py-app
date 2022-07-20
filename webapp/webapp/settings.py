73c923852afcce06089a8fcc1acc98e3e9bbb2f3
JC Guerrero <JCystems@gMail.com>
2022-07-20 10:48:23 -0500

    tweak(package.json)

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -21,11 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+# DEBUG = bool(os.getenv('DEBUG', False))  # FIXME
+DEBUG = bool(os.getenv('DEBUG', True))  # XXX
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')  # NOTE: key not present on docker build!
+if (DEBUG):
+    pprint("DJANGO_SECRET_KEY: %s" % SECRET_KEY)
 
 # ALLOWED_HOSTS = ['0.0.0.0']  # TODO? or XXX?
 ALLOWED_HOSTS = []
